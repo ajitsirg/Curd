9269162864e8bbcfa188ba07b29daba2df7ad74f
ajitsirg <ajittechanical@gmail.com>
2022-11-03 15:20:31 +0530

    vendormodel

--- curd/settings.py
+++ curd/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'profiles',
 ]
 
 MIDDLEWARE = [
@@ -117,7 +118,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+DATE_INPUT_FORMATS = ('%d/%m/%Y')
--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Coustmer(models.Model):
+    name      = models.CharField(max_length=30,blank=True,null=True)
+    age       = models.DateField(auto_now=False, auto_now_add=False,blank=True,null=True)
+    phone     = models.IntegerField(max_length=11,unique=True)
+    email     = models.EmailField(null=True, blank=True)
+    
+    address   = models.TextField(max_length=255,blank=True,null=True)
+    city      = models.CharField(max_length=35,blank=True,null=True)
+    state     = models.CharField(max_length=40,blank=True,null=True)
+    country   = models.CharField(max_length=30 ,blank=True,null=True)
+    
+    def __str__(self):
+        return str(self.name) 
+    
+    
+class Vendor(models.Model):
+    name      = models.CharField(max_length=30,blank=True,null=True)
+    age       = models.DateField(auto_now=False, auto_now_add=False,blank=True,null=True)
+    phone     = models.IntegerField(max_length=11,unique=True)
+    email     = models.EmailField(null=True, blank=True)
+    companyName= models.CharField(max_length=100,blank=True,null=True)
+    gstNumber = models.CharField(max_length=100, blank=True, null=True)
+    panCard   = models.CharField(max_length=10, blank=True, null=True)
+    panImage  = models.ImageField(upload_to='images/', blank=True, null=True )
+    address   = models.TextField(max_length=255,blank=True,null=True)
+    city      = models.CharField(max_length=35,blank=True,null=True)
+    state     = models.CharField(max_length=40,blank=True,null=True)
+    country   = models.CharField(max_length=30 ,blank=True,null=True)
+    
+    def __str__(self):
+        return str(self.name)    
